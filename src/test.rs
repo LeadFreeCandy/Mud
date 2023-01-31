ca7137bb9b438ae92cb62678d505cb912149136d
Samir Beall <samir.beall@gmail.com>
2023-01-30 20:22:02 -0500

    updated tests

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
-use crate::lexer::{Lexer, Lexeme};
+use crate::lexer::{Lexeme, Lexer};
 use crate::*;
 
-fn parse_file(input_filename: &str){
+fn parse_file(input_filename: &str) {
     let input_filename = "mud_tests/".to_owned() + input_filename;
 
     let file = fs::read(input_filename).expect("Unable to open file!");
@@ -10,7 +10,7 @@ fn parse_file(input_filename: &str){
     println!("{:?}", parser.parse().unwrap());
 }
 
-fn lex_file(input_filename: &str){
+fn lex_file(input_filename: &str) {
     let input_filename = "mud_tests/".to_owned() + input_filename;
 
     let file = fs::read(input_filename).expect("Unable to open file!");
@@ -21,38 +21,42 @@ fn lex_file(input_filename: &str){
         let token = lexer.next();
         dbg!(&token);
         if let Ok(Lexeme::Eof) = token {
-            break
+            break;
         }
     }
 }
 
-fn compile_file(input_filename: &str){
+fn compile_file(input_filename: &str) {
     use std::io::prelude::*;
 
     let input_path = "mud_tests/".to_owned() + input_filename;
 
     let in_file = fs::read(&input_path).expect("Unable to open file!");
     // let mut lexer = Lexer::new(file);
-    let program = compiler::compile(in_file).expect("Error Compiling!");
+    let program = compiler::compile(in_file).expect(&format!("Error compiling {input_filename}!"));
 
     let mut output_filename: String = input_filename.split(".").take(1).collect();
     output_filename += ".c";
 
     dbg!(&output_filename);
-    let mut out_file = fs::File::create("mud_tests/target/".to_string() + &output_filename).expect("Unable to create file");
-    out_file.write_all(&program).expect("Unable to write to file");
+    let mut out_file = fs::File::create("mud_tests/target/".to_string() + &output_filename)
+        .expect("Unable to create file");
+    out_file
+        .write_all(&program)
+        .expect("Unable to write to file");
 
+    // let target_filename = "mud_tests/truth/".to_string() + &output_filename;
+    // let target_file = fs::read(target_filename);
 }
 
+#[test]
+fn add_mul() {
+    let filename = "add_mul.mud";
+    compile_file(filename);
+}
 
 #[test]
-fn add_mul(){
-    let filename = "add_mul.mud";
-
-    lex_file(filename);
-    parse_file(filename);
-    compile_file(filename);
-
+fn sequence() {
     let filename = "sequence.mud";
     compile_file(filename);
 }
