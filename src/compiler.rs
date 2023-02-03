5ffa9af5a341781eafecd6a9b6159581f0c7393e
= <=>
2023-02-03 18:24:12 -0500

    started type checking

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,14 +1,99 @@
+use std::ops::Add;
+
 use crate::parser::*;
 use crate::lexer::error::{MudResult, ErrorType};
+
+#[derive(Debug)]
+pub enum Type {
+    Integer,
+    Void,
+}
+
+#[derive(Debug)]
+pub enum CompilerType {
+    Type(Type),
+    Identifier,
+}
+
+struct CompiledAtom {
+    source: String,
+    expr_type: CompilerType,
+}
+
+impl CompiledAtom {
+    fn new(source: String, expr_type: CompilerType) -> Self {
+        Self {
+            source,
+            expr_type,
+        }
+    }
+
+    fn add(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Type(Type::Integer), CompilerType::Type(Type::Integer)) => Ok(self.transform_source(format!("({}+{})", self.source, rhs.source))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+    fn sub(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Type(Type::Integer), CompilerType::Type(Type::Integer)) => Ok(self.transform_source(format!("({}-{})", self.source, rhs.source))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+    fn mul(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Type(Type::Integer), CompilerType::Type(Type::Integer)) => Ok(self.transform_source(format!("({}*{})", self.source, rhs.source))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+
+    fn div(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Type(Type::Integer), CompilerType::Type(Type::Integer)) => Ok(self.transform_source(format!("({}/{})", self.source, rhs.source))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+    fn comp(self, rhs: CompiledAtom) -> MudResult<Self> {
+        Ok(CompiledAtom::new(format!("{};{}", self.source, rhs.source), CompilerType::Type(Type::Void)))
+    }
+
+    fn decl(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Identifier, CompilerType::Identifier) => Ok(CompiledAtom::new(format!("{} {}", self.source, rhs.source), CompilerType::Type(Type::Void))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+    fn assign(self, rhs: CompiledAtom) -> MudResult<Self> {
+        match (self.expr_type, rhs.expr_type) {
+            (CompilerType::Identifier, e) => Ok(CompiledAtom::new(format!("{} {}", self.source, rhs.source), CompilerType::Type(Type::Void))),
+            (l, r) => MudResult::Err(ErrorType::CompileError(format!("Cannot add types {:?} and {:?}", l, r))),
+        }
+    }
+
+    fn negate(self) -> MudResult<Self> {
+        match self.expr_type {
+            CompilerType::Type(Type::Integer) =>
+        }
+    }
+
+    fn transform_source(self, source: String) -> Self {
+        CompiledAtom::new(source, self.expr_type)
+    }
+}
 
 pub fn compile(program: Vec<u8>) -> MudResult<Vec<u8>>{
     let mut parser = Parser::new(program);
     let expression = parser.parse()?;
 
-    let output = convert(expression)?.as_bytes().to_owned();
+    let output = convert(expression)?.source.as_bytes().to_owned();
     Ok(output)
 }
- 
+
 pub fn compile_full(program: Vec<u8>) -> MudResult<Vec<u8>>{
     let output = compile(program)?;
 
@@ -24,34 +109,34 @@ int main() {{
       )
 }
 
-fn binary_op_transpile(op: Operator, lhs: &str, rhs: &str) -> MudResult<String> {
+fn binary_op_transpile(op: Operator, lhs: Expression, rhs: Expression) -> MudResult<CompiledAtom> {
     match op {
-        Operator::Plus => Ok(format!("({lhs}+{rhs})")),
-        Operator::Minus => Ok(format!("({lhs}-{rhs})")),
-        Operator::Asterisk => Ok(format!("({lhs}*{rhs})")),
-        Operator::Semicolon=> Ok(format!("{lhs};\n{rhs}")),
-        Operator::Colon=> Ok(format!("{rhs} {lhs}")),
-        Operator::Equals=> Ok(format!("{lhs} = {rhs}")),
+        Operator::Plus => convert(lhs)?.add(convert(rhs)?),
+        Operator::Minus => convert(lhs)?.sub(convert(rhs)?),
+        Operator::Asterisk => convert(lhs)?.mul(convert(rhs)?),
+        Operator::Semicolon=> convert(lhs)?.comp(convert(rhs)?),
+        Operator::Colon=> convert(lhs)?.decl(convert(rhs)?),
+        Operator::Equals=> convert(lhs)?.assign(convert(rhs)?),
 
         _ => Err(ErrorType::CompileError(format!("Binary operator {:?} cannot be transpiled", op))),
     }
 }
 
-fn unary_op_transpile(op: Operator, oprand: &str) -> MudResult<String> {
+fn unary_op_transpile(op: Operator, oprand: Expression) -> MudResult<String> {
     match op {
-        Operator::Minus => Ok(format!("-({oprand})")),
-        Operator::LessThan => Ok(format!("printf(\"{{}}\", {oprand})")),
+        Operator::Minus => Ok(format!("-({})", convert(oprand)?)),
+        Operator::LessThan => Ok(format!("printf(\"%ll\", {})", convert(oprand)?)),
         _ => Err(ErrorType::CompileError(format!("Unary operator {:?} cannot be transpiled", op))),
     }
 }
 
-fn convert(expression: Expression) -> MudResult<String> {
+fn convert(expression: Expression) -> MudResult<CompiledAtom> {
     match expression {
         Expression::Integer(val) => {
             Ok(val.to_string())
         },
         Expression::Identifier(s) => {
-            Ok(s)
+            Ok(s)n
         }
         Expression::UnaryOperation(op, expr) => {
             unary_op_transpile(op, &convert(*expr)?)
